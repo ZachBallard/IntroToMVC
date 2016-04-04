c1d4997f706da7ed26109aa004320f0bdd2f6b04
Zach Ballard <zbginji@gmail.com>
2016-04-04 16:59:35 -0500

    initial commit

---
+++ IntroToMVC/Controllers/ClassmateController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroToMVC.Controllers
+{
+    public class ClassmateController : Controller
+    {
+        // GET: Classmate
+        public ActionResult Index()
+        {
+
+            var brian = new Classmate("Brian", "Black", 5, 10);
+            var trey = new Classmate("Trey", "Black", 5, 7);
+            var kevin = new Classmate("Kevin", "Black", 5, 10);
+            var tee = new Classmate("Tee", "Black", 5, 10);
+            var kate = new Classmate("Kate", "Brown", 5, 6);
+            var seth = new Classmate("Seth", "Black", 5, 11);
+            var daniel = new Classmate("Daniel", "Brown", 5, 20);
+
+            var classmates = new List<Classmate>() { brian, trey, kevin, tee, kate, seth, daniel };
+
+            return View(classmates);
+        }
+
+        // GET: Classmate/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Classmate/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Classmate/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Classmate/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Classmate/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Classmate/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Classmate/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ IntroToMVC/Models/Classmate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace IntroToMVC
+{
+    public class Classmate
+    {
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+        public int HeightFeet { get; set; }
+        public int HeightInches { get; set; }
+        
+        public Classmate(string name, string hair, int feet, int inches)
+        {
+            Name = name;
+            HairColor = hair;
+            HeightFeet = feet;
+            HeightInches = inches;
+        }
+    }
+}
